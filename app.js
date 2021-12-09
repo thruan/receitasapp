4be7f5caf91675b0a7dd51d0ee5ce96e5b79f3ee
thruan <anthonyrbm08@gmail.com>
2021-12-08 21:54:01 -0300

    Utlima

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// importar models
+const users = require('./models/user.model')
+
+mongoose.connect('mongodb://localhost:27017/Cookmaster', {
+    //opções
+}).then(() => {
+    console.log('Conectado com sucesso ao banco de dados')   
+}).catch((err) => {
+    console.log(err)
+});
 
 app.get('/', (req,res) => {
     return res.status(200).json({
-        mensagem: 'Rota de tipo GET com endereço'
+        mensagem: 'Rota de tipo GET com endereço',
+        data: '08/12/2021',
+        author: 'Anthony Ruan',
+        location: 'Senai, Imperatriz-MA'
+    })
+});
+
+app.post('/novoUsuario', (req,res) => {
+    const {name, email, password, role} = req.body;
+    console.log(name, email, password, role);
+    
+    return res.status(201).json({
+        mensagem: 'Usuário cadastrado com sucesso'
     })
 })
 
